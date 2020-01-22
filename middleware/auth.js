647b8d518e0272f0476828d0f97d95b77b543949
David Sellam <dvdsellam@gmail.com>
2020-01-22 14:21:03 +0200

    deploy

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 const config = require("config");
 
-module.exports = function(req, res, next) {
+module.exports = (req, res, next) => {
   // Get the token to the header
   const token = req.header("x-auth-token");
 
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const connectDB = require("./config/db");
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 // connect DB
 connectDB();
 
