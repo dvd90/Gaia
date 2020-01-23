45fb2fd601160aa29640ba8d5dadd772c903fdc9
David Sellam <dvdsellam@gmail.com>
2020-01-23 19:21:03 +0200

    try deployment

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/db");
 const app = express();
+const path = require("path");
 
 const PORT = process.env.PORT || 4000;
 // connect DB
@@ -9,12 +10,22 @@ connectDB();
 
 // Init Middleware
 app.use(express.json({ extended: false }));
-app.get("/", (req, res) => res.send("API Running"));
+// app.get("/", (req, res) => res.send("API Running"));
 
 // Define routes
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/challenges", require("./routes/api/challenges"));
 app.use("/api/events", require("./routes/api/events"));
+
+// FOR HEROKU DEPLOYMENT
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
